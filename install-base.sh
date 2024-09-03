59a3b08ab2e31e9c407d500f5025a5c24bc05b5e
Alexander Pletzer <alexander@gokliya.net>
2024-09-04 10:52:06 +1200

    manullay install cylc

--- install-base.sh
+++ install-base.sh
@@ -163,6 +163,9 @@ mkdir -p /opt/metomi-site/etc/rose
 dos2unix -n /vagrant/opt/metomi-site/etc/rose/rose.conf /opt/metomi-site/etc/rose/rose.conf
 
 #### Install latest versions of FCM, Cylc & Rose
+
+
+
 if [[ $dist == ubuntu ]]; then
   # Ensure curl is installed
   apt-get install -q -y curl || error
@@ -173,10 +176,11 @@ dos2unix -n /vagrant/usr/local/bin/install-cylc8 /usr/local/bin/install-cylc8
 dos2unix -n /vagrant/usr/local/bin/install-rose /usr/local/bin/install-rose
 /usr/local/bin/install-fcm --set-default || error
 # /usr/local/bin/install-cylc7 --set-default --make-docs || error
-/usr/local/bin/install-cylc8 || error
+# does not seem to install
+# /usr/local/bin/install-cylc8 || error
 /usr/local/bin/install-rose --set-default --make-docs || error
 # Set the default to Cylc 8
-ln -sf /opt/cylc-8 /opt/cylc
+# ln -sf /opt/cylc-8 /opt/cylc
 
 # #### Configure syntax highlighting & bash completion
 # sudo -u $(logname) mkdir -p /home/vagrant/.local/share/gtksourceview-3.0/language-specs/
